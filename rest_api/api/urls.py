1bd14643bbb02ac520b49f16a490ef2772046253
S-rayhan-S <s.rayhansiddikey@gmail.com>
2025-04-26 21:34:30 +0600

    Learnet about the mixins part of Django-REST frame work

--- rest_api/api/urls.py
+++ rest_api/api/urls.py
@@ -1,16 +1,14 @@
 
 from django.urls import path
-from status.views import (StatusViewer,StatusListView,StatusCreateView,StatusDetailView,StatusUpdateView,StatusDeleteView)
+# from status.views import (StatusViewer,StatusListView,StatusCreateView,StatusDetailView,StatusUpdateView,StatusDeleteView)
+
+from status.views import StatusListCreateApiView,StatusDetailDeleteUpdateView
 
 
 urlpatterns = [
    
-    path('statuses/',StatusListView.as_view(),name='status_view'),
-    path('status/create/',StatusCreateView.as_view(),name='status_list_view'),
+    path('status/',StatusListCreateApiView.as_view(),name='status_view'),
+    path('status/<pk>/',StatusDetailDeleteUpdateView.as_view(),name=''),
     
-    path('status/<pk>/',StatusUpdateView.as_view(),name=''),
-    path('status/update/<pk>/',StatusUpdateView.as_view(),name=''),
-    path('status/delete/<pk>/',StatusDeleteView.as_view(),name=''),
-   
     
 ]
--- rest_api/status/views.py
+++ rest_api/status/views.py
@@ -4,39 +4,57 @@ from rest_framework.response import Response
 
 from .models import Status
 from .serializers import StatusSerializer
-from rest_framework import generics
+from rest_framework import generics,mixins
 
 # Create your views here.
-class StatusViewer(APIView):
-    def get(self,request,**kwargs):
-        id=kwargs.get('id')
-        status=Status.objects.get(pk=id)
-        serializer= StatusSerializer(status, many=False)
-        return Response(serializer.data)
-
-class StatusListView(generics.ListAPIView):
-    # def get(self,request):
-    #     all_status=Status.objects.all()
-    #     serializer=StatusSerializer(all_status,many=True)
-    #     return Response(serializer.data)
+class StatusListCreateApiView(generics.ListAPIView,mixins.CreateModelMixin):
     queryset=Status.objects.all()
     serializer_class=StatusSerializer
     
-
-class StatusCreateView(generics.CreateAPIView):
+    def post(self, request, *args, **kwargs):
+       return self.create(request, *args, **kwargs)
+    
+class StatusDetailDeleteUpdateView(generics.RetrieveAPIView,mixins.DestroyModelMixin,mixins.UpdateModelMixin):
     queryset=Status.objects.all()
     serializer_class=StatusSerializer
     
-class StatusDetailView(generics.RetrieveAPIView):
-    queryset=Status.objects.all()
-    serializer_class=StatusSerializer   
+    def delete(self,request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
     
-class StatusUpdateView(generics.UpdateAPIView):
-    queryset=Status.objects.all()
-    serializer_class=StatusSerializer   
+    def update(self,request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+
+# class StatusViewer(APIView):
+#     def get(self,request,**kwargs):
+#         id=kwargs.get('id')
+#         status=Status.objects.get(pk=id)
+#         serializer= StatusSerializer(status, many=False)
+#         return Response(serializer.data)
+
+# class StatusListView(generics.ListAPIView):
+#     # def get(self,request):
+#     #     all_status=Status.objects.all()
+#     #     serializer=StatusSerializer(all_status,many=True)
+#     #     return Response(serializer.data)
+#     queryset=Status.objects.all()
+#     serializer_class=StatusSerializer
     
-class StatusDeleteView(generics.DestroyAPIView):  
-    queryset=Status.objects.all()
-    serializer_class=StatusSerializer   
+
+# class StatusCreateView(generics.CreateAPIView):
+#     queryset=Status.objects.all()
+#     serializer_class=StatusSerializer
+    
+# class StatusDetailView(generics.RetrieveAPIView):
+#     queryset=Status.objects.all()
+#     serializer_class=StatusSerializer   
+    
+# class StatusUpdateView(generics.UpdateAPIView):
+#     queryset=Status.objects.all()
+#     serializer_class=StatusSerializer   
+    
+# class StatusDeleteView(generics.DestroyAPIView):  
+#     queryset=Status.objects.all()
+#     serializer_class=StatusSerializer   
 
 
